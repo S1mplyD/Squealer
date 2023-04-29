afae2ccac1d6b2f2bcba9dce0c45a58354ce6c57
d3lusional1990 <97901909+d3lusional1990@users.noreply.github.com>
2023-04-29 11:39:25 +0200

    Inizializzato Front Office Squealer

---
+++ squealer-fo/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'Squealer Front Office';
+
+  httpError!: HttpErrorResponse;
+
+  constructor(private router: Router, private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+  }
+}
