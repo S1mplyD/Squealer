c3530e87d5c034b9358dda297a9f5956e6d0b47c
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-05-06 11:43:18 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import session from "express-session";
+import mongoose from "mongoose";
+
+config();
+
+const maxAge: number = 24 * 60 * 60 * 1000;
+const app = express();
+const uri: string = process.env.MONGO_TEST!;
+const port: any = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.COOKIE_KEY!,
+    cookie: { maxAge: maxAge },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(uri);
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
