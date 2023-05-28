4f11fd6b58d757187565279bbbfa5cde4fab628f
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-05-28 16:02:41 +0200

    add new types

---
+++ backend/util/types.ts
@@ -0,0 +1,30 @@
+// nuovo tipo Squeal
+export type Squeal = {
+  body: string; //Corpo del messaggio (testo, immagine (path), video (path), geolocazione (coordinate per open street map api))
+  recipients: string[]; //Destinatari (individuo, canale o keyword)
+  date: Date; //Data e ora messaggio
+  positiveReactions: number | undefined;
+  negativeReactions: number | undefined;
+  category: string; //Categorie (privato, pubblico, popolare, impopolare, controverso)
+  channels: string[] | undefined; //Canali a cui è stato aggiunto dalla redazione
+  //Non visualizzabili
+  criticalMass: number | undefined; //Massa critica (0,25 x visual)
+  visual: number | undefined; //Visualizzazioni di account registrati e non
+};
+
+// nuovo tipo per squeal temporizzati
+export type TimedSqueal = {
+  body: string; //Corpo del messaggio (testo, immagine (path), video (path), geolocazione (coordinate per open street map api))
+  recipients: string[]; //Destinatari (individuo, canale o keyword)
+  date: Date; //Data e ora messaggio
+  positiveReactions: number | undefined;
+  negativeReactions: number | undefined;
+  category: string; //Categorie (privato, pubblico, popolare, impopolare, controverso)
+  channels: string[] | undefined; //Canali a cui è stato aggiunto dalla redazione
+  //Non visualizzabili
+  criticalMass: number | undefined; //Massa critica (0,25 x visual)
+  visual: number | undefined; //Visualizzazioni di account registrati e non
+  time: number | undefined; //tempo per i messaggi automatici (in ms)
+  intervalId: number | undefined; // id dell'intervallo
+  count: number | undefined;
+};
