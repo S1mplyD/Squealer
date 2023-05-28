a3d950d3611186debbe79bdd0708471b4c143f31
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-05-28 16:03:41 +0200

    remove timer fields from normal squeal

--- backend/database/models/squeals.model.ts
+++ backend/database/models/squeals.model.ts
@@ -13,8 +13,6 @@ const squealSchema = new Schema(
     //Non visualizzabili
     criticalMass: { type: Number }, //Massa critica (0,25 x visual)
     visual: { type: Number, default: 0, required: true }, //Visualizzazioni di account registrati e non
-    time: { type: Number }, //tempo per i messaggi automatici (in ms)
-    intervalId: { type: Number }, // id dell'intervallo
   },
   { collection: "squealData" }
 );
