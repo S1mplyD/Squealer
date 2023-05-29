b0988ae3cf892ee5c2389573ba9699cfecd774e3
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-05-29 11:07:38 +0200

    remove success from errors

--- backend/util/errors.ts
+++ backend/util/errors.ts
@@ -10,14 +10,12 @@ export class Error {
 
 // enum per il codice degli errori
 export enum ErrorCodes {
-  success = 0,
   cannot_create = 1,
   non_existent = 2,
   cannot_update = 3,
 }
 
 export enum ErrorDescriptions {
-  success = "Success",
   cannot_create = "Cannot create!",
   non_existent = "Nothing found!",
   cannot_update = "Cannot update!",
