19fb87c1ac382354973af919750ddae21cc628a3
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-04 17:42:54 +0200

    add new defaults

--- backend/database/models/squeals.model.ts
+++ backend/database/models/squeals.model.ts
@@ -6,12 +6,12 @@ const squealSchema = new Schema(
     body: { type: String, required: true }, //Corpo del messaggio (testo, immagine (path), video (path), geolocazione (coordinate per open street map api))
     recipients: { type: [String], required: true }, //Destinatari (individuo, canale o keyword)
     date: { type: Date, required: true }, //Data e ora messaggio
-    positiveReactions: { type: Number },
-    negativeReactions: { type: Number },
+    positiveReactions: { type: Number, default: 0 },
+    negativeReactions: { type: Number, default: 0 },
     category: { type: String, required: true }, //Categorie (privato, pubblico, popolare, impopolare, controverso)
     channels: { type: [String] }, //Canali a cui è stato aggiunto dalla redazione
     //Non visualizzabili
-    criticalMass: { type: Number }, //Massa critica (0,25 x visual)
+    criticalMass: { type: Number, default: 0 }, //Massa critica (0,25 x visual)
     visual: { type: Number, default: 0, required: true }, //Visualizzazioni di account registrati e non
   },
   { collection: "squealData" }
