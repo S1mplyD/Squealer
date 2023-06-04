6680e97c2db33487a87c4f8211204270f655352d
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-04 16:24:28 +0200

    add channels routes

--- backend/routes/channels.ts
+++ backend/routes/channels.ts
@@ -3,20 +3,50 @@ import {
   getAllChannels,
   addSquealToChannel,
   createChannel,
+  deleteChannel,
 } from "../database/querys/channels";
+import { Error, ErrorCodes, ErrorDescriptions } from "../util/errors";
 
 export const router = express.Router();
 
 /**
  * GET
  * ritorna tutti i canali
+ * TESTATA
  */
-router.route("/").get(async (req, res) => {
-  try {
-    await getAllChannels().then((channels) => {
-      res.send(channels);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      await getAllChannels().then((channels) => {
+        res.send(channels);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * POST
+   * crea un nuovo canale
+   * TESTATO
+   */
+  .post(async (req, res) => {
+    try {
+      await createChannel(req.query.name as string).then((ret) => {
+        res.send(ret);
+      });
+    } catch (error: any) {
+      console.log({ errorName: error.name, errorDescription: error.message });
+      res.send({ errorName: error.name, errorDescription: error.message });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      await deleteChannel(req.query.name as string).then((ret) => {
+        res.send(ret);
+      });
+    } catch (error: any) {
+      console.log({ errorName: error.name, errorDescription: error.message });
+      res.send({ errorName: error.name, errorDescription: error.message });
+    }
+  });
