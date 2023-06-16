46626cb098d3525c4cb9f42ab3406603944a9e1c
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-16 18:27:49 +0200

    create vite react app

---
+++ smm_dashboard/src/Dashboard.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+const Dashboard: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <div
+      className={`container mx-auto px-4 sm:px-6 lg:px-8 ${
+        darkMode ? "bg-purple-700" : "bg-purple-200"
+      } rounded-lg`}
+    >
+      <nav className="flex items-center justify-between py-4 ">
+        <div>
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+        </div>
+        <div>
+          <button
+            className="p-4 py-2 rounded-lg bg-blue-500 text-white"
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? <FiSun /> : <FiMoon />}
+            {/* <span className="ml-2">
+              {darkMode ? "Light Mode" : "Dark Mode"}
+            </span> */}
+          </button>
+        </div>
+      </nav>
+
+      <div
+        className={`py-4 rounded-lg ${
+          darkMode ? "bg-purple-700" : "bg-purple-200"
+        }`}
+      >
+        <ul className="flex flex-wrap justify-center space-x-2 sm:justify-center sm:space-x-4">
+          <li>
+            <button className="btn-link bg-purple-500 text-white rounded-lg p-4 m-2 w-[200px] ">
+              Create New Squeal
+            </button>
+          </li>
+          <li>
+            <button className="btn-link bg-purple-500 text-white rounded-lg p-4 m-2 w-[200px]">
+              Analytics
+            </button>
+          </li>
+          <li>
+            <button className="btn-link bg-purple-500 text-white rounded-lg p-4 m-2 w-[200px]">
+              Geolocation
+            </button>
+          </li>
+          <li>
+            <button className="btn-link bg-purple-500 text-white rounded-lg p-4 m-2 w-[200px]">
+              Buy More Character
+            </button>
+          </li>
+          <li>
+            <button className="btn-link bg-purple-500 text-white rounded-lg p-4 m-2 w-[200px]">
+              Add/Remove SMM
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
