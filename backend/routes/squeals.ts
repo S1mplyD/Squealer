02c141a52aa152a1bdd6eb515ef971d42d47cc40
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-24 12:12:34 +0200

    add delete function and fixes

--- backend/routes/squeals.ts
+++ backend/routes/squeals.ts
@@ -13,7 +13,7 @@ import {
 } from "../database/querys/squeals";
 import express from "express";
 import { startTimer } from "../util/timers";
-import { TimedSqueal } from "../util/types";
+import { TimedSqueal, Error } from "../util/types";
 export const router = express.Router();
 
 router
@@ -26,19 +26,6 @@ router
     try {
       const squeals: any = await getAllSqueals();
       res.status(200).send(squeals);
-    } catch (error) {
-      console.log(error);
-    }
-  })
-  /**
-   * DELETE
-   * Elimina uno squeal
-   */
-  .delete(async (req, res) => {
-    try {
-      await deleteSqueal(req.query.id as string).then((ret) => {
-        res.send(ret);
-      });
     } catch (error) {
       console.log(error);
     }
@@ -66,6 +53,19 @@ router
         req.query.filename as string
       );
       res.send(newSqueal);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * DELETE
+   * Elimina uno squeal
+   */
+  .delete(async (req, res) => {
+    try {
+      await deleteSqueal(req.query.id as string).then((ret) => {
+        res.send(ret);
+      });
     } catch (error) {
       console.log(error);
     }
@@ -133,8 +133,10 @@ router
    */
   .get(async (req, res) => {
     try {
-      const squeals: any = await getSquealsByRecipients(req.body.recipients);
-      res.status(200).send(squeals);
+      const squeals: any = await getSquealsByRecipients(
+        req.query.recipient as string
+      );
+      res.send(squeals);
     } catch (error) {
       console.log(error);
     }
@@ -144,12 +146,14 @@ router
   .route("/channels")
   /**
    * GET
-   * ritorna tutti gli squeal appartenenti ai canali ricercati
+   * ritorna tutti gli squeal appartenenti ad un canale
    */
   .get(async (req, res) => {
     try {
-      const squeals: any = await getSquealsByChannel(req.body.channels);
-      res.status(200).send(squeals);
+      const squeals: any = await getSquealsByChannel(
+        req.query.channel as string
+      );
+      res.send(squeals);
     } catch (error) {
       console.log(error);
     }
@@ -163,7 +167,8 @@ router
    */
   .get(async (req, res) => {
     try {
-      const timedSqueals: any = await getAllTimers();
+      const timedSqueals: TimedSqueal[] | Error | undefined =
+        await getAllTimers();
       res.send(timedSqueals);
     } catch (error: any) {
       console.log({ errorName: error.name, errorDescription: error.message });
@@ -176,9 +181,7 @@ router
    */
   .post(async (req, res) => {
     try {
-      const time: number = req.query.time as unknown as number;
       const squeal: TimedSqueal = req.body.squeal;
-      const id: string = req.query.id as string;
       startTimer(squeal);
     } catch (error) {
       console.log(error);
