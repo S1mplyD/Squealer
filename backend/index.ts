9b6e8f273c78932ba43d23e2578ed9d8e92ed024
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-26 17:37:36 +0200

    add new routes

--- backend/index.ts
+++ backend/index.ts
@@ -9,6 +9,10 @@ import { router as authRoute } from "./routes/authentication";
 import { startAllTimer } from "./util/timers";
 import { router as channelRoute } from "./routes/channels";
 import { router as squealsRoute } from "./routes/squeals";
+import { router as squealMediaRoute } from "./routes/squealMedia";
+import { router as squealGeoRoute } from "./routes/squealGeo";
+import { router as squealTimedRoute } from "./routes/timedSqueal";
+import { router as mediaRoute } from "./routes/media";
 
 config();
 
@@ -36,6 +40,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/auth", authRoute);
 app.use("/api/channels", channelRoute);
 app.use("/api/squeals", squealsRoute);
+app.use("/api/squeals/media", squealMediaRoute);
+app.use("/api/squeals/geo", squealGeoRoute);
+app.use("/api/squeals/timed", squealTimedRoute);
+app.use("/api/media", mediaRoute);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(uri).then(async () => {
