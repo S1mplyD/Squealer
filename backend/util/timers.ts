696e496420529a8e1929d553824f573465c23cc4
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-09 18:01:00 +0200

    add error and undefined management

--- backend/util/timers.ts
+++ backend/util/timers.ts
@@ -10,11 +10,16 @@ import { TimedSqueal } from "./types";
  * funzione che attiva tutti i timer (da usare all'avvio del server)
  */
 export async function startAllTimer() {
-  const squeals: any = await getAllTimers();
-  if (!squeals.code) {
-    for (let i of squeals) {
-      await startTimer(i as TimedSqueal);
-    }
+  const squeals: TimedSqueal[] | Error | undefined = await getAllTimers();
+  if (squeals instanceof Error) {
+    return squeals;
+  }
+  if (squeals) {
+    if (squeals.length > 0) {
+      for (let i of squeals) {
+        await startTimer(i as TimedSqueal, i.author);
+      }
+    } else return new Error(ErrorDescriptions.no_timers, ErrorCodes.no_timers);
   } else return new Error(ErrorDescriptions.no_timers, ErrorCodes.no_timers);
 }
 
@@ -24,16 +29,16 @@ export async function startAllTimer() {
  * @param time il tempo necessario per postare uno squeal
  * @param id id dello squeal
  */
-export async function startTimer(squeal: TimedSqueal) {
-  const interval: any = setInterval(async () => {
-    await postTimedSqueal(squeal);
+export async function startTimer(squeal: TimedSqueal, author: string) {
+  const interval: NodeJS.Timer = setInterval(async () => {
+    await postTimedSqueal(squeal, author);
   }, squeal.time);
   await setSquealInterval(squeal, interval);
 }
 
 /**
  * funzione che ferma un post automatico
- * @param squeal squeal automatico da fermare
+ * @param TimedSqueal squeal automatico da fermare
  */
 export async function stopTimer(squeal: TimedSqueal) {
   clearInterval(squeal.intervalId);
