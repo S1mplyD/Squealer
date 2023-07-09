6b166a0227f94da3e8515f580e470102a98b524e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-09 15:39:56 +0200

    add filetypes constants

---
+++ backend/util/constants.ts
@@ -0,0 +1,4 @@
+const imagetypes: Array<string> = ["jpg", "jpeg", "png", "gif", "webp", "svg"];
+const videotypes: Array<string> = ["mp4", "webm", "ogv", "ogg"];
+
+export { imagetypes, videotypes };
