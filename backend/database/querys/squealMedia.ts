278521343e7ee9686717d3b592539ae8ad76b747
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-09 15:41:38 +0200

    add file type checking

--- backend/database/querys/squealMedia.ts
+++ backend/database/querys/squealMedia.ts
@@ -1,3 +1,4 @@
+import { imagetypes, videotypes } from "../../util/constants";
 import { ErrorCodes, ErrorDescriptions, Error } from "../../util/errors";
 import { Success, SuccessCode, SuccessDescription } from "../../util/success";
 import { SquealMedia } from "../../util/types";
@@ -14,7 +15,7 @@ const publicUploadPath = resolve(__dirname, "../..", "public/uploads/");
  */
 export async function getMediaSqueals() {
   try {
-    const squeals: any[] = await squealMediaModel.find();
+    const squeals: SquealMedia[] = await squealMediaModel.find();
     if (squeals.length < 1)
       return new Error(ErrorDescriptions.non_existent, ErrorCodes.non_existent);
     else return squeals;
@@ -26,14 +27,32 @@ export async function getMediaSqueals() {
 /**
  * funzione che crea un media squeal
  * @param squeal corpo dello squeal da creare
- * @param filename nome del file (utilizzare la chiamata di caricamento e poi mettere in "filename" il valore di ritorno)
+ * @param filename {string} nome del file (utilizzare la chiamata di caricamento e poi mettere in "filename" il valore di ritorno)
  * @returns ritorna successo o errore
  */
 export async function postMediaSqueal(squeal: SquealMedia, filename: string) {
   try {
     const channels: any = await getAllChannels();
+    var type: string | undefined = filename.split(".").pop();
+
+    if (typeof type === "undefined") {
+      return new Error(ErrorDescriptions.not_recived, ErrorCodes.not_recived);
+    } else {
+      if (imagetypes.includes(type)) {
+        type = "image";
+      } else if (videotypes.includes(type)) {
+        type = "video";
+      } else {
+        return new Error(
+          ErrorDescriptions.not_supported,
+          ErrorCodes.not_supported,
+        );
+      }
+    }
+
     const newSqueal: any = await squealMediaModel.create({
       body: filename,
+      type: type,
       recipients: squeal.recipients,
       date: new Date(),
       category: squeal.category,
@@ -43,7 +62,7 @@ export async function postMediaSqueal(squeal: SquealMedia, filename: string) {
     if (!newSqueal)
       return new Error(
         ErrorDescriptions.cannot_create,
-        ErrorCodes.cannot_create
+        ErrorCodes.cannot_create,
       );
     else {
       if (newSqueal.channels.length < 1) {
@@ -79,18 +98,18 @@ export async function deleteMediaSqueal(id: string) {
         console.log(err);
         return new Error(
           ErrorDescriptions.cannot_delete,
-          ErrorCodes.cannot_delete
+          ErrorCodes.cannot_delete,
         );
       }
     });
     const deleted: any = await squealMediaModel.deleteOne(
       { _id: id },
-      { returnDocument: "after" }
+      { returnDocument: "after" },
     );
     if (deleted.deletedCount < 1)
       return new Error(
         ErrorDescriptions.cannot_delete,
-        ErrorCodes.cannot_delete
+        ErrorCodes.cannot_delete,
       );
     else return new Success(SuccessDescription.removed, SuccessCode.removed);
   } catch (error: any) {
