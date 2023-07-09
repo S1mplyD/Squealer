d975a39592f97ce0ef61340534594eb5bf54b191
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-09 15:42:43 +0200

    add start functions

---
+++ backend/routes/analytics.ts
@@ -0,0 +1,30 @@
+import chartjs from "chart.js"
+
+/**
+ * funzione che ritorna le visualizzazioni di un post
+ * @params id {string}
+ */
+export async function getPostVisual(id: string){
+
+}
+
+/**
+ * funzione che ritorna un grafico delle visualizzazioni del post
+ * @params id {string} id del post
+ */
+export async function getPostVisualChart(id: string){
+
+}
+
+/**
+ * funzione che ritorna le visualizzazioni di tutti i post
+ * @params id {string} id dell'utente 
+ *
+ */
+export async function getAllPostsVisuals(id: string){
+
+}
+
+export async function getPostImpressions(id: string){
+
+}
