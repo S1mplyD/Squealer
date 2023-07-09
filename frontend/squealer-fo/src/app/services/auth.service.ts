6ef0d4e29e38b71536886ea8a03dc64fd6d9e598
d3lusional1990 <97901909+d3lusional1990@users.noreply.github.com>
2023-07-09 16:47:12 +0200

    Fix

--- frontend/squealer-fo/src/app/services/auth.service.ts
+++ frontend/squealer-fo/src/app/services/auth.service.ts
@@ -6,7 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
+<<<<<<< Updated upstream
   private apiUrl = 'http://localhost:3000/api/auth'; // Replace with your authentication API URL
+=======
+  private apiUrl = 'https://your-auth-api.com'; // Replace with your authentication API URL
+>>>>>>> Stashed changes
 
   constructor(private http: HttpClient) {}
 
@@ -14,7 +18,12 @@ export class AuthService {
     // Implement your login with Google logic here
     // You can make a POST request to your authentication API to initiate the Google login process
     // Example:
+<<<<<<< Updated upstream
     return this.http.post(`${this.apiUrl}/google`, {});
+=======
+    return this.http.post(`${this.apiUrl}/login/google`, {});
+
+>>>>>>> Stashed changes
   }
 
   loginWithEmail(email: string, password: string): Observable<any> {
@@ -24,18 +33,30 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/login`, { email, password });
   }
 
+<<<<<<< Updated upstream
   signUp(email: string, password: string, name: string, username: string): Observable<any> {
     // Implement your sign up logic here
     // You can make a POST request to your authentication API with the new user's email and password
     // Example:
     return this.http.post(`${this.apiUrl}/register`, { email, password, name, username });
+=======
+  signUp(email: string, password: string): Observable<any> {
+    // Implement your sign up logic here
+    // You can make a POST request to your authentication API with the new user's email and password
+    // Example:
+    return this.http.post(`${this.apiUrl}/signup`, { email, password });
+>>>>>>> Stashed changes
   }
 
   recoverPassword(email: string): Observable<any> {
     // Implement your password recovery logic here
     // You can make a POST request to your authentication API to send a password reset email
     // Example:
+<<<<<<< Updated upstream
     return this.http.post(`${this.apiUrl}/forgotPassword`, { email });
+=======
+    return this.http.post(`${this.apiUrl}/recover-password`, { email });
+>>>>>>> Stashed changes
   }
 
   logout(): Observable<any> {
