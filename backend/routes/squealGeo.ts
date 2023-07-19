840f551ec89b9957410f50a8c7f3c82b24c27d4e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-19 09:00:45 +0200

    add authentication checks

--- backend/routes/squealGeo.ts
+++ backend/routes/squealGeo.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import {
   deleteGeoSqueal,
+  getGeoSqueal,
   getGeoSqueals,
   postGeoSqueal,
 } from "../database/querys/squealGeo";
-import { Error, SquealGeo, User } from "../util/types";
+import { Error, SquealGeo, Success, User } from "../util/types";
+import { unauthorized } from "../util/errors";
+import mongoose from "mongoose";
 
 export const router = express.Router();
 
@@ -31,10 +34,10 @@ router
       if (req.user) {
         const ret: any = await postGeoSqueal(
           req.body,
-          (req.user as User).username,
+          (req.user as User).username
         );
         res.send(ret);
-      }
+      } else res.send(unauthorized);
     } catch (error: any) {
       res.send({ errorName: error.name, errorDescription: error.message });
     }
@@ -43,11 +46,32 @@ router
    * DELETE
    * chiamata per rimuovere un geo squeal
    */
+  //TODO controllo per SMM
   .delete(async (req, res) => {
     try {
-      await deleteGeoSqueal(req.query.id as string).then((ret) => {
-        res.send(ret);
-      });
+      // Controllo se l'utente è loggato
+      if (!req.user) res.send(unauthorized);
+      // Controllo se l'utente è admin
+      // Se è admin posso cancellare qualsiasi squeal
+      else if ((req.user as User).plan == "admin") {
+        const returnValue: Error | Success | undefined = await deleteGeoSqueal(
+          req.query.id as string
+        );
+        res.send(returnValue);
+      } else {
+        //Se l'utente non è admin allora controllo che sia l'autore dello squeal e poi cancello
+        const squeal: SquealGeo | Error = await getGeoSqueal(
+          req.query.id as unknown as mongoose.Types.ObjectId
+        );
+        if (squeal instanceof Error) return squeal;
+        else {
+          if ((squeal as SquealGeo).author === (req.user as User).username) {
+            const returnValue: Error | Success | undefined =
+              await deleteGeoSqueal(req.query.id as string);
+            res.send(returnValue);
+          } else res.send(unauthorized);
+        }
+      }
     } catch (error: any) {
       res.send({ errorName: error.name, errorDescription: error.message });
     }
--- backend/routes/timedSqueal.ts
+++ backend/routes/timedSqueal.ts
@@ -3,10 +3,13 @@ import { Error, TimedSqueal, User } from "../util/types";
 import {
   deleteTimedSqueal,
   getAllTimers,
+  getTimedSqueal,
   postTimedSqueal,
 } from "../database/querys/timedSqueal";
 import { startTimer } from "../util/timers";
 import { Success } from "../util/success";
+import { unauthorized } from "../util/errors";
+import mongoose from "mongoose";
 
 export const router = express.Router();
 
@@ -31,16 +34,19 @@ router
    */
   .post(async (req, res) => {
     try {
-      const squeal: TimedSqueal = req.body;
-      const newSqueal: TimedSqueal = await postTimedSqueal(
-        squeal,
-        (req.user as User).username
-      );
-      const ret: Error | Success = await startTimer(
-        newSqueal,
-        (req.user as User).username
-      );
-      res.send(ret);
+      if (!req.user) res.send(unauthorized);
+      else {
+        const squeal: TimedSqueal = req.body;
+        const newSqueal: TimedSqueal = await postTimedSqueal(
+          squeal,
+          (req.user as User).username
+        );
+        const ret: Error | Success = await startTimer(
+          newSqueal,
+          (req.user as User).username
+        );
+        res.send(ret);
+      }
     } catch (error: any) {
       res.send({ errorName: error.name, errorDescription: error.message });
     }
@@ -51,10 +57,26 @@ router
    */
   .delete(async (req, res) => {
     try {
-      const ret: Error | Success | undefined = await deleteTimedSqueal(
-        req.query.id as string
-      );
-      res.send(ret);
+      if (!req.user) res.send(unauthorized);
+      else if ((req.user as User).plan === "admin") {
+        const ret: Error | Success | undefined = await deleteTimedSqueal(
+          req.query.id as string
+        );
+        res.send(ret);
+      } else {
+        //Se l'utente non è admin allora controllo che sia l'autore dello squeal e poi cancello
+        const squeal: TimedSqueal | Error = await getTimedSqueal(
+          req.query.id as unknown as mongoose.Types.ObjectId
+        );
+        if (squeal instanceof Error) return squeal;
+        else {
+          if ((squeal as TimedSqueal).author === (req.user as User).username) {
+            const returnValue: Error | Success | undefined =
+              await deleteTimedSqueal(req.query.id as string);
+            res.send(returnValue);
+          } else res.send(unauthorized);
+        }
+      }
     } catch (error: any) {
       res.send({ errorName: error.name, errorDescription: error.message });
     }
