08291446e6ea0c51275fc5f768ddbd18ab2869d9
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-19 09:00:59 +0200

    add plan types

--- backend/util/constants.ts
+++ backend/util/constants.ts
@@ -1,4 +1,11 @@
 const imagetypes: Array<string> = ["jpg", "jpeg", "png", "gif", "webp", "svg"];
 const videotypes: Array<string> = ["mp4", "webm", "ogv", "ogg"];
+const plans: Array<string> = [
+  "base",
+  "verficato",
+  "professional",
+  "journalist",
+  "admin",
+];
 
-export { imagetypes, videotypes };
+export { imagetypes, videotypes, plans };
