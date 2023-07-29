2b222d7f2c801851bfc0bea43ec47e903b542868
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-29 17:32:04 +0200

    add default characters set for user plans

--- backend/util/constants.ts
+++ backend/util/constants.ts
@@ -2,10 +2,23 @@ const imagetypes: Array<string> = ["jpg", "jpeg", "png", "gif", "webp", "svg"];
 const videotypes: Array<string> = ["mp4", "webm", "ogv", "ogg"];
 const plans: Array<string> = [
   "base",
-  "verficato",
+  "verfied",
   "professional",
   "journalist",
   "admin",
 ];
 
-export { imagetypes, videotypes, plans };
+const defaultCharactersBase = [300, 2000, 7500];
+const defaultCharactersVerified = [600, 4000, 15000];
+const defaultCharactersProfessional = [1000, 6500, 25000];
+const defaultCharactersJournalist = [3000, 20000, 75000];
+
+export {
+  imagetypes,
+  videotypes,
+  plans,
+  defaultCharactersBase,
+  defaultCharactersJournalist,
+  defaultCharactersProfessional,
+  defaultCharactersVerified,
+};
