1c052d8e406fe1c0c32d8bfcbf827969e8c20c24
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-07-29 17:43:20 +0200

    add: characters checks

--- backend/database/querys/automatedSqueal.ts
+++ backend/database/querys/automatedSqueal.ts
@@ -1,13 +1,14 @@
 import automatedSquealModel from "../models/automatedSqueal.model";
+import { cannot_create, no_characters, non_existent } from "../../util/errors";
 import {
+  AutomatedSqueal,
+  Id,
+  Success,
+  TimedSqueal,
   Error,
-  ErrorCodes,
-  ErrorDescriptions,
-  cannot_create,
-  non_existent,
-} from "../../util/errors";
-import { AutomatedSqueal, TimedSqueal } from "../../util/types";
-import mongoose from "mongoose";
+} from "../../util/types";
+import { updateCount } from "./timedSqueal";
+import { getUserCharacter, updateDailyCharacters } from "../../util/characters";
 
 export async function getAllAutomatedSqueals() {
   const squeals: AutomatedSqueal[] = await automatedSquealModel.find();
@@ -15,20 +16,53 @@ export async function getAllAutomatedSqueals() {
   else return squeals;
 }
 
+/**
+ * funzione che posta uno squeal automatico
+ * @param squeal TimedSqueal da postare in automatico
+ * @param originalSqueal Id dello squeal originale
+ * @param userId id utente
+ * @returns Error | AutomatedSqueal
+ */
 export async function postAutomatedSqueal(
   squeal: TimedSqueal,
-  originalSqueal: mongoose.Types.ObjectId
+  originalSqueal: Id,
+  userId: Id,
 ) {
-  console.log("automated post");
-  const newSqueal: AutomatedSqueal = await automatedSquealModel.create({
-    body: squeal.body,
-    recipients: squeal.recipients,
-    date: new Date(),
-    category: squeal.category,
-    author: squeal.author,
-    originalSqueal: originalSqueal,
-    channels: squeal.channels,
-  });
-  if (!newSqueal) return cannot_create;
-  else return newSqueal;
+  const result: [number, number, number] | Error = await getUserCharacter(
+    userId,
+  );
+
+  if (result instanceof Error) return result;
+  else {
+    const [dailyCharacters, weeklyCharacters, monthlyCharacters] = result as [
+      number,
+      number,
+      number,
+    ];
+    if (dailyCharacters > squeal.body.length) {
+      const newSqueal: AutomatedSqueal = await automatedSquealModel.create({
+        body: squeal.body,
+        recipients: squeal.recipients,
+        date: new Date(),
+        category: squeal.category,
+        author: squeal.author,
+        originalSqueal: originalSqueal,
+        channels: squeal.channels,
+      });
+      if (!newSqueal) return cannot_create;
+      else {
+        const updateCharacter: Error | Success = await updateDailyCharacters(
+          userId,
+          newSqueal.body.length,
+        );
+        if (updateCharacter instanceof Error) return updateCharacter;
+        const count: Error | Success = await updateCount(squeal._id);
+        if (count instanceof Error) {
+          return count;
+        } else {
+          return newSqueal;
+        }
+      }
+    } else return no_characters;
+  }
 }
