d729339d3093c9dbdfd28d30800b58b429ed7d7a
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-08-18 16:56:10 +0200

    Create analytics.model.ts

---
+++ backend/database/models/analytics.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model, Document } from "mongoose";
+
+// analitiche
+interface Analytics {
+  squealId: string; //id dello squeal
+  // Date di raccolta statistiche (e.g. ogni 3 ore aggiungo la data in cui viene effettuata la raccolta statistiche)
+  dates?: Date[];
+  // numero di visualizzazione in una determinata data
+  // visuals[0] sarà il numero di visualizzazione nella data dates[0]
+  visuals?: number[];
+  // numero di reazioni positive in una determinata data
+  // positiveReactions[0] sarà il numero di reazioni positive nella data dates[0]
+  positiveReactions?: number[];
+  // numero di reazioni negative in una determinata data
+  // negativeReactions[0] sarà il numero di reazioni negative nella data dates[0]
+  negativeReactions?: number[];
+}
+
+interface AnalyticsDocument extends Document, Analytics {}
+
+const analyticsSchema = new Schema<AnalyticsDocument>({
+  squealId: { type: String, required: true, unique: true },
+  dates: { type: [Date] },
+  visuals: { type: [Number] },
+  positiveReactions: { type: [Number] },
+  negativeReactions: { type: [Number] },
+});
+
+const analyticsDataModel = model<AnalyticsDocument>(
+  "analyticsData",
+  analyticsSchema
+);
+
+export default analyticsDataModel;
