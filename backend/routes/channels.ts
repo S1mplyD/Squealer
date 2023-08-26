82b9f38c807e5cabbfb9ffff4abb4c311dcfb458
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-08-26 16:54:44 +0200

    add new channels routes

--- backend/routes/channels.ts
+++ backend/routes/channels.ts
@@ -1,50 +1,72 @@
 import express from "express";
 import {
   getAllChannels,
-  addSquealToChannel,
   createChannel,
   deleteChannel,
+  getAllSquealsFromChannel,
+  getChannel,
+  getAllUserChannel,
+  getAllOfficialChannel,
+  getAllKeywordChannel,
+  getAllMentionChannel,
+  addUserToUserChannel,
+  removeUserFromChannel,
 } from "../database/querys/channels";
+import { SquealerError, catchError, unauthorized } from "../util/errors";
 import {
-  ErrorCodes,
-  ErrorDescriptions,
-  SquealerError,
-  catchError,
-  unauthorized,
-} from "../util/errors";
-import { Success, User } from "../util/types";
-import { plans } from "../util/constants";
+  Channel,
+  Squeal,
+  SquealGeo,
+  SquealMedia,
+  Success,
+  TimedSqueal,
+  TimedSquealGeo,
+  User,
+} from "../util/types";
+import { getAllUserAnalytics } from "../database/querys/analytics";
 
 export const router = express.Router();
 
 /**
  * GET
- * ritorna tutti i canali
- * TESTATA
+ * getAllChannels
  */
 router
   .route("/")
   .get(async (req, res) => {
     try {
-      await getAllChannels().then((channels) => {
+      if (req.user && (req.user as User).status !== "ban") {
+        const channels: Channel[] | SquealerError = await getAllChannels();
         res.send(channels);
-      });
+      }
     } catch (error: any) {
       catchError(error);
     }
   })
   /**
    * POST
-   * crea un nuovo canale
-   * TESTATO
+   * createChannel
    */
   .post(async (req, res) => {
     try {
       if ((req.user as User).plan === "admin") {
         const returnValue: SquealerError | Success = await createChannel(
-          req.query.name as string
+          req.query.name as string,
+          req.query.type as string,
+          req.user as User
         );
         res.send(returnValue);
+      } else if (
+        req.user &&
+        ((req.user as User).status !== "ban" ||
+          (req.user as User).status !== "block")
+      ) {
+        const newChannel: SquealerError | Success = await createChannel(
+          req.query.name as string,
+          req.query.type as string,
+          req.user as User
+        );
+        res.send(newChannel);
       } else {
         res.send(unauthorized);
       }
@@ -54,14 +76,17 @@ router
   })
   /**
    * DELETE
-   * elimina un canale
-   * TESTATA
+   * deleteChannel
    */
   .delete(async (req, res) => {
     try {
-      if ((req.user as User).plan === "admin") {
+      if (
+        (req.user as User).status !== "block" ||
+        (req.user as User).status !== "ban"
+      ) {
         const returnValue: SquealerError | Success = await deleteChannel(
-          req.query.name as string
+          req.query.name as string,
+          req.user as User
         );
         res.send(returnValue);
       } else {
@@ -73,22 +98,166 @@ router
   });
 
 router
-  .route("/addSqueal")
+  .route("/:name")
+  /**
+   * GET
+   * getChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const channel: Channel | SquealerError = await getChannel(
+          req.params.name
+        );
+        res.send(channel);
+      } else res.send(unauthorized);
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/userchannel")
+  /**
+   * GET
+   * getAlluserChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const userchannel: Channel[] | SquealerError = await getAllUserChannel(
+          req.user as User
+        );
+        res.send(userchannel);
+      } else res.send(unauthorized);
+    } catch (error) {
+      catchError(error);
+    }
+  });
+router
+  .route("/userchannel/add")
   /**
    * POST
-   * aggiungi uno squeal ad un canale
-   * TESTATA
+   * addUserToUserChannel
    */
   .post(async (req, res) => {
     try {
-      if ((req.user as User).plan === "admin") {
-        const returnValue: SquealerError | Success = await addSquealToChannel(
-          req.body.channelName!,
-          req.body.squealId!
+      if (
+        (req.user as User).status !== "ban" ||
+        (req.user as User).status !== "block"
+      ) {
+        const retValue: SquealerError | Success = await addUserToUserChannel(
+          req.body.username,
+          req.body.channelName
         );
-        res.send(returnValue);
+        if (retValue instanceof SquealerError) {
+          if (retValue.code === 20) res.sendStatus(404);
+          else res.sendStatus(500);
+        } else res.sendStatus(200);
+      }
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/userchannel/remove")
+  /**
+   * POST
+   * addUserToUserChannel
+   */
+  .post(async (req, res) => {
+    try {
+      if (
+        (req.user as User).status !== "ban" ||
+        (req.user as User).status !== "block"
+      ) {
+        const retValue: SquealerError | Success = await removeUserFromChannel(
+          req.body.username,
+          req.body.channelName
+        );
+        if (retValue instanceof SquealerError) {
+          if (retValue.code === 20) res.sendStatus(404);
+          else res.sendStatus(500);
+        } else res.sendStatus(200);
+      }
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/official")
+  /**
+   * GET
+   * getAllOfficialChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const officialChannels: Channel[] | SquealerError =
+          await getAllOfficialChannel();
+        res.send(officialChannels);
       } else res.send(unauthorized);
-    } catch (error: any) {
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/keyword")
+  /**
+   * GET
+   * getAllKeywordChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const keywordChannel: Channel[] | SquealerError =
+          await getAllKeywordChannel();
+        res.send(keywordChannel);
+      } else res.send(unauthorized);
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/mention")
+  /**
+   * GET
+   * getAllMentionChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const mentionChannel: Channel[] | SquealerError =
+          await getAllMentionChannel();
+        res.send(mentionChannel);
+      } else res.send(unauthorized);
+    } catch (error) {
+      catchError(error);
+    }
+  });
+
+router
+  .route("/squeals")
+  /**
+   * GET
+   * getAllSquealsFromChannel
+   */
+  .get(async (req, res) => {
+    try {
+      if ((req.user as User).status !== "ban") {
+        const squeals:
+          | (Squeal | SquealGeo | SquealMedia | TimedSqueal | TimedSquealGeo)[]
+          | SquealerError = await getAllSquealsFromChannel(
+          req.query.name as string,
+          (req.user as User)._id
+        );
+        res.send(squeals);
+      } else res.send(unauthorized);
+    } catch (error) {
       catchError(error);
     }
   });
