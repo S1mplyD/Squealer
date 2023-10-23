34e2daac7da6c80a0a18e38c76aa7dd4d616b277
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-10-23 18:05:06 +0200

    feat: add getManagedUsers http call

--- smm_dashboard/src/HTTPcalls.ts
+++ smm_dashboard/src/HTTPcalls.ts
@@ -1,42 +1,48 @@
 import axios from "axios";
-import { Squeal, User } from "./utils/types";
+import { Squeal } from "./utils/types";
 const path: string = "http://localhost:3000";
 
 export async function postSqueal(
-    body: string,
-    category: string,
-    channels: string[],
-    type: string,
-    lng?: string,
-    lat?: string,
-    time?: number,
-    recipients?: string[],
+  body: string,
+  category: string,
+  channels: string[],
+  username: string,
+  type: string,
+  lng?: string,
+  lat?: string,
+  time?: number,
+  recipients?: string[]
 ) {
-    const requestBody = {
-        body: body,
-        lat: lat,
-        lng: lng,
-        recipients: recipients,
-        category: category,
-        channels: channels,
-        type: type,
-        time: time,
-    };
-    const newSqueal = await axios.post<Squeal>(
-        path + "/api/squeals/type",
-        requestBody,
-    );
-    console.log(newSqueal);
-
-    return newSqueal;
+  const requestBody = {
+    body: body,
+    lat: lat,
+    lng: lng,
+    recipients: recipients,
+    category: category,
+    channels: channels,
+    type: type,
+    time: time,
+  };
+  const newSqueal = await axios.post<Squeal>(
+    `${path}/api/squeals/smm/${username}`,
+    requestBody
+  );
+  return newSqueal;
 }
 
 export async function getUser(username: string) {
-    const user = await axios.get(path + `/api/users/user/${username}`);
-    return user.data;
+  const user = await axios.get(path + `/api/users/user/${username}`);
+  return user.data;
 }
 
 export async function getMe() {
-    const user: User = await axios.get(path + "/api/users/me");
-    return user;
+  const user = await axios.get(path + "/api/users/me");
+  return user.data;
+}
+
+export async function getManagedUsers(username: string) {
+  const managedUsers = await axios.get(
+    `${path}/api/users/managedUsers/${username}`
+  );
+  return managedUsers.data;
 }
