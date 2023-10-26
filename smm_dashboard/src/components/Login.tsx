fefa448235f069e336d410d3550a5a3737c1c451
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-10-26 12:00:22 +0200

    fix: fix login not adding mail to request

--- smm_dashboard/src/components/Login.tsx
+++ smm_dashboard/src/components/Login.tsx
@@ -1,35 +1,44 @@
+import { login } from "../HTTPcalls";
+
 const Login: React.FC = () => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col bg-orange p-4 m-4 items-center rounded-xl w-fit justify-center">
-        <form action="http://127.0.0.1:3000/api/auth/login" method="post">
-          <div className="p-4 m-4 flex flex-col items-center">
-            <label htmlFor="mail" className="my-3">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="mail"
-              id="mail"
-              className="p-2 rounded-lg"
-            ></input>
-          </div>
-          <div className="p-4 m-4 flex flex-col items-center">
-            <label htmlFor="password" className="my-3">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              id="password"
-              className="rounded-lg p-2"
-            />
-          </div>
-          <div className="p-4 m-4 flex flex-col items-center text-white bg-grey rounded-xl">
-            <input type="submit" value="Login" className="" />
-          </div>
-        </form>
+        <div className="p-4 m-4 flex flex-col items-center">
+          <label htmlFor="mail" className="my-3">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="mail"
+            id="mail"
+            className="p-2 rounded-lg"
+          ></input>
+          <label htmlFor="password" className="my-3">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            id="password"
+            className="rounded-lg p-2"
+          />
+        </div>
+        <div className="p-4 m-4 flex flex-col items-center text-white bg-grey rounded-xl">
+          <button
+            onClick={() => {
+              login(
+                (document.getElementById("mail") as HTMLInputElement).value,
+                (document.getElementById("password") as HTMLInputElement).value
+              ).then(() => {
+                location.reload();
+              });
+            }}
+          >
+            Login
+          </button>
+        </div>
         <div className="m-4">OR</div>
         <div>
           <button className=" rounded-xl bg-grey p-4 m-4 text-white">
