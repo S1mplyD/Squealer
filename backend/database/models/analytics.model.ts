5209080a74822e1246031d42c53946365718c340
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-11-28 10:45:20 +0100

    feat: add squeals fields to analytic model

--- backend/database/models/analytics.model.ts
+++ backend/database/models/analytics.model.ts
@@ -3,34 +3,39 @@ import { Schema, model, Document } from "mongoose";
 // analitiche
 interface Analytics {
   squealId: string; //id dello squeal
-  // Date di raccolta statistiche (e.g. ogni 3 ore aggiungo la data in cui viene effettuata la raccolta statistiche)
-  dates?: Date[];
+  body: string;
+  lat?: string;
+  lng?: string;
+  type: string;
+  //Data di creazione dell'analitica (ogni giorno)
+  date: Date;
   // numero di visualizzazione in una determinata data
-  // visuals[0] sarà il numero di visualizzazione nella data dates[0]
-  visuals?: number[];
+  visuals: number;
   // numero di reazioni positive in una determinata data
-  // positiveReactions[0] sarà il numero di reazioni positive nella data dates[0]
-  positiveReactions?: number[];
+  positiveReactions: number;
   // numero di reazioni negative in una determinata data
-  // negativeReactions[0] sarà il numero di reazioni negative nella data dates[0]
-  negativeReactions?: number[];
-  author: string; //id dell'autore dello squeal
+  negativeReactions: number;
+  author: string; //username dell'autore dello squeal
 }
 
 interface AnalyticsDocument extends Document, Analytics {}
 
 const analyticsSchema = new Schema<AnalyticsDocument>({
   squealId: { type: String, required: true, unique: true },
-  dates: { type: [Date] },
-  visuals: { type: [Number] },
-  positiveReactions: { type: [Number] },
-  negativeReactions: { type: [Number] },
+  body: { type: String },
+  lat: { type: String },
+  lng: { type: String },
+  type: { type: String },
+  date: { type: Date },
+  visuals: { type: Number },
+  positiveReactions: { type: Number },
+  negativeReactions: { type: Number },
   author: { type: String, required: true },
 });
 
 const analyticsDataModel = model<AnalyticsDocument>(
   "analyticsData",
-  analyticsSchema
+  analyticsSchema,
 );
 
 export default analyticsDataModel;
