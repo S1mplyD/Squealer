038b49c1756fbf86dd5463d6afa5cfe8e349c9ae
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-12-03 11:41:10 +0100

    style: add commment

--- backend/database/models/users.model.ts
+++ backend/database/models/users.model.ts
@@ -36,7 +36,7 @@ const userSchema = new Schema<UserDocument>({
   monthlyCharacters: { type: Number, required: true, default: 7500 }, //Caratteri mensili
   plan: { type: String, default: "base", required: true }, //Tipo di account (base, [verificato], professional, journalist, admin)
   SMM: { type: String }, // {username} SMM dell´account, modificabile solo se l'account è professional
-  managedAccounts: { type: [String] }, //Account gestiti da un SMM, modificabile se il plan è pro (usermane)
+  managedAccounts: { type: [String] }, //Account gestiti da un SMM, modificabile se il plan è pro (username)
   resetToken: { type: String, default: "" },
   followers: { type: [String] },
   following: { type: [String] },
