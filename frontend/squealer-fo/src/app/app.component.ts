9d5b5b388e37ac02fe2c4a489eec4eec227d9558
d3lusional1990 <97901909+d3lusional1990@users.noreply.github.com>
2023-12-17 16:58:25 +0100

    Reso layout più responsive

--- frontend/squealer-fo/src/app/app.component.ts
+++ frontend/squealer-fo/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
+import { BreakpointObserver,Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Subject, takeUntil } from 'rxjs';
 
 @Component({
@@ -24,22 +25,35 @@ export class AppComponent implements OnInit, OnDestroy{
   private _unsubscribeAll: Subject<void> = new Subject<void>();
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService: AuthService, private router: Router) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private authService: AuthService,
+    private router: Router,
+    public responsive: BreakpointObserver) {
+      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      this.mobileQuery.addListener(this._mobileQueryListener);
   }
-  ngOnInit(): void {
-    this.authService.isAuthenticated()
-    .pipe(takeUntil(this._unsubscribeAll))
-    .subscribe((res) => {
-      if (res.status !== '404') {
-        this.isLoggedIn = true;
-        this.username = res.username + '';
-        this.plan = res.plan + '';
-      }
-    });
-  }
+
+ngOnInit(): void {
+  this.responsive
+  .observe([Breakpoints.HandsetPortrait])
+  .subscribe((state: BreakpointState) => {
+    if (state.matches) {
+      console.log('This is the Handset Portrait point at max-width: 599.98 px and portrait orientation.');
+    }
+  });
+  this.authService.isAuthenticated()
+  .pipe(takeUntil(this._unsubscribeAll))
+  .subscribe((res) => {
+    if (res.status !== '404') {
+      this.isLoggedIn = true;
+      this.username = res.username + '';
+      this.plan = res.plan + '';
+    }
+  });
+}
 
 logout(): void {
   this.authService.logout()
