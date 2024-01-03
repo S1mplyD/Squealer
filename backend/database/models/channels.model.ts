40b49ad36c479c5320fd36aef0d12b347863ac49
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-03 11:46:52 +0100

    style

--- backend/database/models/channels.model.ts
+++ backend/database/models/channels.model.ts
@@ -14,7 +14,7 @@ interface ChannelDocument extends Channel, Document {}
 const channelsSchema = new Schema<ChannelDocument>({
   name: { type: String, required: true, unique: true }, //Nome del canale (§canale, §CANALE, #keyword)
   squeals: { type: [String] }, //id dei messaggi con destinatario il canale
-  //​@user, §userchannel, §ADMINCHANNEL, #keyword
+  //@user, §userchannel, §ADMINCHANNEL, #keyword
   type: { type: String, required: true },
   allowedRead: { type: [String] }, //id degli utenti che possono accedere al canale
   allowedWrite: { type: [String] },
