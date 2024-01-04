deca2d9e1caf8db113cead25928d7546b0b4e712
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-04 15:55:24 +0100

    feat: add http call to get a user using username and to block a user

--- frontend/squealer-bo/HTTPCalls/users.js
+++ frontend/squealer-bo/HTTPCalls/users.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
 const getAll = async () => {
-  const response = await axios("/api/users");
+  const response = await axios.get("/api/users");
   return response.data;
 };
 
-module.exports = { getAll };
+const getByUsername = async (username) => {
+  const response = await axios.get(`/api/users/user/${username}`);
+  return response.data;
+};
+
+const blockUser = async (username, time) => {
+  const response = await axios.post("/api/users/block", null, {
+    params: { username, time },
+  });
+  return response.data;
+};
+
+module.exports = { getAll, getByUsername };
