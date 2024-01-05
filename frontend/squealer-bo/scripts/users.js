7efe61731df9bf1e8c58ef7653498eb39580e38f
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-05 22:39:52 +0100

    feat: add user table

--- frontend/squealer-bo/scripts/users.js
+++ frontend/squealer-bo/scripts/users.js
@@ -1,11 +1,46 @@
-import userFunctions from "../HTTPCalls/users";
-
-const createUserTable = async () => {
-  const users = await userFunctions.getAll();
-  const newMain = document.getElementById("usertable");
-  newMain.html(
-    "<table id='table'><tr><th>Name</th><th>Username</th></tr></table>",
-  );
+const getAll = async () => {
+  const response = await axios.get("/api/users");
+  return response.data;
 };
 
-module.exports = { createUserTable };
+const getByUsername = async (username) => {
+  const response = await axios.get(`/api/users/user/${username}`);
+  return response.data;
+};
+
+const blockUser = async (username, time) => {
+  const response = await axios.post("/api/users/block", null, {
+    params: { username, time },
+  });
+  return response.data;
+};
+
+const unblockUser = async (username) => {
+  const response = await axios.post("/api/users/unblock", null, {
+    params: { username },
+  });
+};
+
+const createUserTable = async () => {
+  const users = await getAll();
+  const newMain = document.getElementById("usertable");
+  newMain.html(
+    "<table id='table'>" +
+      "<tr><th>Name</th><th>Username</th><th>Mail</th><th>Plan</th><th>Status</th><th>Daily Characters</th><th>Weekly Characters</thWee><th>Monthly Characters</th></tr>" +
+      "</table>",
+  );
+  for (let i of users) {
+    newMain.append(
+      "<tr>" +
+        "<td>i.name</td>" +
+        "<td>i.username</td>" +
+        "<td>i.mail</td>" +
+        "<td>i.plan</td>" +
+        "<td>i.status</td>" +
+        "<td>i.dailycharacters</td>" +
+        "<td>i.weeklycharacters</td>" +
+        "<td>i.monthlycharacters</td>" +
+        "</tr>",
+    );
+  }
+};
