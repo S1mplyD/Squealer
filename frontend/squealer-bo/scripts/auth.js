e35b5cb63e4d86737ba00665395138e994b51076
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-05 17:50:28 +0100

    feat: add login checks
    add a check to determinate if the user is logged in and display the login form if necessary

--- frontend/squealer-bo/scripts/auth.js
+++ frontend/squealer-bo/scripts/auth.js
@@ -1,10 +1,28 @@
-import auth from "../HTTPCalls/auth";
-const getLoggedUser = async () => {
-  return await auth.getMe();
-};
+async function getMe() {
+  const main = document.getElementById("main");
+  const response = await axios.get("/api/users/me").catch((err) => {
+    console.log("here");
+    if (err.response.status === 404) {
+      main.innerHTML =
+        "<h1>Login</h1>" +
+        "<form>" +
+        '<label for="mail">E-mail:</label><input type="email" name="mail" id="mail" placeholder="E-mail" />' +
+        '<label for="password">Password:</label> <input type="password" name="password" id="password" placeholder="Password"/>' +
+        '<button type="button" value="login" onclick="login()">Login</button>' +
+        "</form>";
+    }
+  });
 
-const login = async (mail, password) => {
-  return await auth.login(mail, password);
-};
+  if (response) {
+    main.innerHTML = "<h1>Welcome " + response.data.username + "</h1>";
+  }
+}
 
-module.exports = { getLoggedUser, login };
+async function login() {
+  const response = await axios.post("/api/auth/login", {
+    username: document.getElementById("mail").value,
+    password: document.getElementById("password").value,
+  });
+  document.location.reload();
+  return response.data;
+}
