4b71e3d0f1adae3d50a7ab5c0dbe9bd873585310
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-05 15:38:38 +0100

    feat: add starting table

--- frontend/squealer-bo/scripts/users.js
+++ frontend/squealer-bo/scripts/users.js
@@ -2,6 +2,10 @@ import userFunctions from "../HTTPCalls/users";
 
 const createUserTable = async () => {
   const users = await userFunctions.getAll();
+  const newMain = document.getElementById("usertable");
+  newMain.html(
+    "<table id='table'><tr><th>Name</th><th>Username</th></tr></table>",
+  );
 };
 
 module.exports = { createUserTable };
