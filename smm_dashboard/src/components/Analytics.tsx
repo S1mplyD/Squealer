2114773a977037a786326d25c092116e2669a926
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-25 12:25:45 +0100

    refactor: remove unused analytics

--- smm_dashboard/src/components/Analytics.tsx
+++ smm_dashboard/src/components/Analytics.tsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
-import { Analytic, User } from "../utils/types.ts";
-import AnalyticComponent from "./Analytic.tsx";
-import { getMe, getManagedUsers, getAllUserAnalytics } from "../HTTPcalls.ts";
+import { User } from "../utils/types.ts";
+import { getMe, getManagedUsers } from "../HTTPcalls.ts";
 
 export function Analytics() {
-  const [userAnalytics, setUserAnalytics] = useState<Analytic[]>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [user, setUser] = useState<User>();
   const [managedUsers, setManagedUsers] = useState<User[]>();
   const [selectedUser, setSelectedUser] = useState<string>();
+
   useEffect(() => {
     async function fetchData() {
       const user: User = await getMe();
       setUser(user);
       if (user.managedAccounts && user.managedAccounts.length > 0) {
         const managedUsers = await getManagedUsers(user.username);
+        managedUsers.push(user);
         setManagedUsers(managedUsers);
-        const analytics = await getAllUserAnalytics(managedUsers[0].username);
-        setUserAnalytics(analytics);
       }
     }
     fetchData().then(() => {
@@ -25,19 +23,11 @@ export function Analytics() {
     });
   }, []);
 
-  const changeAnalytics = async () => {
-    if (selectedUser) {
-      const analytics = await getAllUserAnalytics(selectedUser);
-      setUserAnalytics(analytics);
-    }
-  };
-
   const handleChange = () => {
     const selectedValue = (
       document.getElementById("manageduserselect") as HTMLSelectElement
     ).value;
     setSelectedUser(selectedValue);
-    changeAnalytics();
   };
 
   if (!isLoading && user && user.plan === "professional") {
@@ -58,17 +48,6 @@ export function Analytics() {
               ))}
             </select>
           ) : null}
-          <div>
-            {userAnalytics ? (
-              <div className={"flex flex-col"}>
-                {userAnalytics.map((userAnalytic) => (
-                  <AnalyticComponent
-                    analytic={userAnalytic}
-                  ></AnalyticComponent>
-                ))}
-              </div>
-            ) : null}
-          </div>
         </div>
       </>
     );
