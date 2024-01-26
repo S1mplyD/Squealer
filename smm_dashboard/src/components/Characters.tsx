f5f8687cf94a5f2429df5b0d6b238689b866c2e2
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-26 15:40:44 +0100

    feat: add characters component

---
+++ smm_dashboard/src/components/Characters.tsx
@@ -0,0 +1,13 @@
+export function Characters() {
+  const buyCharacters = async () => {};
+  return (
+    <div>
+      <div>Buy More character</div>
+      <input type="number" placeholder="Daily characters" />
+      <input type="number" placeholder="Daily characters" />
+      <input type="number" placeholder="Daily characters" />
+      <div>The prica is {price}</div>
+      <button onClick={buyCharacters}>BUY</button>
+    </div>
+  );
+}
