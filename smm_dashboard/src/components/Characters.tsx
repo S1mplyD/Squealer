e003e7b11ca3fb870475f76c731630e893ef0055
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-01-27 12:02:10 +0100

    feat: add buyCharacters function

--- smm_dashboard/src/components/Characters.tsx
+++ smm_dashboard/src/components/Characters.tsx
@@ -1,14 +1,30 @@
 import { useState, useEffect } from "react";
 import { User } from "../utils/types";
-import { getMe } from "../HTTPcalls";
+import { buyCharactersForUser, getManagedUsers, getMe } from "../HTTPcalls";
 
 export function Characters() {
   const [user, setUser] = useState<User>();
   const [price, setPrice] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
+  const [selectedUser, setSelectedUser] = useState<User>();
+
   let pricePerCharacter = 0.05;
 
-  const buyCharacters = async () => {};
+  const buyCharacters = async () => {
+    if (selectedUser) {
+      let daily = document.getElementById("daily") as HTMLInputElement;
+      let weekly = document.getElementById("weekly") as HTMLInputElement;
+      let monthly = document.getElementById("monthly") as HTMLInputElement;
+      const response = await buyCharactersForUser(
+        selectedUser.username,
+        +daily.value,
+        +weekly.value,
+        +monthly.value,
+      );
+      if (response.status === 200) return true;
+      else return false;
+    }
+  };
 
   const getPrice = () => {
     let daily = document.getElementById("daily") as HTMLInputElement;
@@ -23,6 +39,9 @@ export function Characters() {
     const fetchData = async () => {
       const user: User = await getMe();
       setUser(user);
+      const managedUsers: User[] = await getManagedUsers(user.username);
+      managedUsers.push(user);
+      setSelectedUser(managedUsers[0]);
     };
 
     fetchData().then(() => {
@@ -59,7 +78,19 @@ export function Characters() {
           onChange={getPrice}
         />
         <div>The price is {price.toFixed(2)}€</div>
-        <button onClick={buyCharacters}>BUY</button>
+        <button
+          onClick={async () => {
+            const result = await buyCharacters();
+            if (result) {
+              alert(
+                `Characters correctly added to user ${selectedUser?.username}`,
+              );
+              window.location.reload();
+            } else alert("Something went wrong");
+          }}
+        >
+          BUY
+        </button>
       </div>
     );
   }
